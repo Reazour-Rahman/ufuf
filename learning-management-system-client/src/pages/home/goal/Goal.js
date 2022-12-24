c002746663767a2ae112379c7a89a42924420dcf
Reazour Rahaman Rabbi <alexrabbi111@gmail.com>
2022-12-24 20:38:46 +0600

    subject

--- learning-management-system-client/src/pages/home/goal/Goal.js
+++ learning-management-system-client/src/pages/home/goal/Goal.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import {Box, Typography, Grid} from '@mui/material'
+import React from "react";
+import { Box, Typography, Grid, useTheme } from "@mui/material";
+import { AiFillApple } from "react-icons/ai";
+import Boundary from "../../../components/Boundary";
+
+const goals = [
+  { title: "প্রোগ্রামিং", sub: "তোমার দক্ষতা বৃদ্ধি করে" },
+  { title: "ব্যবসায় শিক্ষা", sub: "ব্যবসা হলে সফল" },
+  { title: "মেশিন লার্নিং", sub: "সবকিছু মেশিন" },
+  { title: "ওয়েব 3.O ব্লকচেইন", sub: "আধুনিক টেকনোলজি" },
+  { title: "হেলথ & ফিটনেস", sub: "স্বাস্থ্য সম্পদ" },
+  { title: "ইউআই ইউএক্স ডিসাইন", sub: "ভাল নকশা স্পষ্ট" },
+  { title: "3D মডেলিং", sub: "তোমার সৃজনশীলতা দেখাও" },
+  { title: "মার্কেটিং এনালাইসিস", sub: "যুক্তি হলো মুক্তি" },
+];
 
 const Goal = () => {
-    return (
+  /* use Hooks and states */
+  const theme = useTheme();
+  /* ::::::::::::::Styles:::::::::::::::: */
+  const gridItem = {
+    backgroundColor: theme.palette.common.white,
+    borderRadius: "15px",
+    p: 3,
+    textAlign: "center",
+  };
+
+  return (
+    <Boundary>
+      <Box sx={{ mt: { md: 12, sm: 12, xs: 7 } }}>
         <Box>
-            <Typography>আপনার লক্ষ্য নির্বাচন করুন</Typography>
+          <Grid container spacing={3}>
+            {goals.map((e, index) => (
+              <Grid key={index} item xs={6} sm={4} md={3}>
+                <Box sx={{ ...gridItem }}>
+                  <AiFillApple style={{fontSize:"26px"}} />
+                  <Typography variant="h3">{e.title}</Typography>
+                  <Typography variant="subtitle1">{e.sub}</Typography>
+                </Box>
+              </Grid>
+            ))}
+          </Grid>
         </Box>
-    );
+      </Box>
+    </Boundary>
+  );
 };
 
 export default Goal;
--- learning-management-system-client/src/pages/home/index.js
+++ learning-management-system-client/src/pages/home/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import Banner from './banner/Banner';
-import { styled } from '@mui/material/styles';
+import Goal from './goal/Goal';
 
-const Container = styled('div')(({ theme }) => ({
-    display: 'flex',
-    gap: '30px',
-    marginTop: '14px'
-}));
 const index = () => {
     return (
         <div>
             <Banner/>
+            <Goal/>
         </div>
     );
 };
-/* <Box sx={{ width: '1180px', maxWidth: '99%', margin: '0 auto' }}>{children}</Box> */
 export default index;
